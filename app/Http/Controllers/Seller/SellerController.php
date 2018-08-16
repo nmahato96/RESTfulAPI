e8e0e1e7bdc5b3b7206314af920a616fbbb06ffe
niladri <nmahato@aapnainfotech.in>
2018-08-16 16:20:34 +0530

    restful api

---
+++ app/Http/Controllers/Seller/SellerController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Seller;
+
+use App\Http\Controllers\Controller;
+use App\Seller;
+use Illuminate\Http\Request;
+
+class SellerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $sellers=Seller::has('products')->get();
+        return response()->json(['data'=> $sellers], 200);
+    }
+
+   
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $seller=Seller::has('products')->findOrFail($id);
+        return response()->json(['data'=> $seller], 200);
+    }
+
+   
+}
